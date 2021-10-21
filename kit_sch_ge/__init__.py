3cdf1fb8e1141a9325e196471cc4460ce46e8913
sjeknic <sjeknic@stanford.edu>
2021-10-21 13:33:51 -0700

    added __init__ to fix imports in kit_sch_ge

---
+++ kit_sch_ge/__init__.py
@@ -0,0 +1,7 @@
+import sys
+import os
+
+"""TODO: Would prefer to do this without having to change sys.path
+couldn't quite figure it out. Maybe relative imports"""
+
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
