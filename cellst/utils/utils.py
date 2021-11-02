8aa5142641d47ddfb159beb40fafef13806559f5
sjeknic <sjeknic@stanford.edu>
2021-11-02 10:46:34 -0700

    Fixed bug in ImageHelper and simplified

--- cellst/utils/utils.py
+++ cellst/utils/utils.py
@@ -1,7 +1,7 @@
 import types
 import inspect
 import functools
-from typing import List, Callable
+from typing import List, Tuple
 
 import numpy as np
 
@@ -63,26 +63,32 @@ class ImageHelper():
     def __call__(self, func):
         # Get expected type from function annotation
         self.output_type = inspect.signature(func).return_annotation
+        self.func = func
 
-        @functools.wraps(func)
+        @functools.wraps(self.func)
         def wrapper(*args, **kwargs):
-            if not isinstance(args[0], (list, np.ndarray)):
-                # This means the method is not static
-                pass_to_func, nargs, nkwargs = self._type_helper(func,
-                                                                 *args[1:],
-                                                                 **kwargs)
-                pass_to_func.insert(0, args[0])
-            else:
-                pass_to_func, nargs, nkwargs = self._type_helper(func,
-                                                                 *args,
-                                                                 **kwargs)
+            # Find all of the non-image related inputs (e.g. class, self, etc.)
+            # Save the nonarr_inputs and pass others to _type_helper
+            nonarr_idx = 0
+            while True:
+                if not isinstance(args[nonarr_idx], (list, np.ndarray)):
+                    nonarr_idx += 1
+                else:
+                    break
+            nonarr_inputs = args[:nonarr_idx]
+            args = args[nonarr_idx:]
+
+            # Sort the inputs and keep only those that are relevant
+            pass_to_func, nargs, nkwargs = self._type_helper(*args, **kwargs)
 
             # Function expects and returns a stack.
             if not self.by_frame or len(pass_to_func) == 0:
-                stack = func(*pass_to_func, *nargs, **nkwargs)
+                stack = self.func(*nonarr_inputs, *pass_to_func,
+                                  *nargs, **nkwargs)
             else:
-                stack = self._pass_by_frames(func, pass_to_func,
+                stack = self._pass_by_frames(pass_to_func, nonarr_inputs,
                                              *nargs, **kwargs)
+
             # NOTE: won't work for 1D arrays. Does that matter?
             while stack.ndim < 3 and self.output_type in (Image, Mask, Track):
                 stack = np.expand_dims(stack, axis=-1)
@@ -90,19 +96,21 @@ class ImageHelper():
             return self.output_type, stack
         return wrapper
 
-    def _type_helper(self, func, imgs, msks, trks, arrs, *args, **kwargs):
-        '''
+    def _type_helper(self, imgs, msks, trks, arrs, *args, **kwargs):
+        """
         This func is for sorting the input types and selecting the correct
         types that should get passed to the function.
-        '''
+        """
         # Check which inputs the function is expecting and only pass those
         expected_types = [i.annotation.__name__
-                          for i in inspect.signature(func).parameters.values()
+                          for i in inspect.signature(self.func).parameters.values()
                           if hasattr(i, __name__)]
         expected_names = [p.name
-                          for p in inspect.signature(func).parameters.values()]
+                          for p in inspect.signature(self.func).parameters.values()]
 
-        inpt_bools = [(i in expected_types) or (i in expected_names)
+        # Check for what the function expects. Include plurals for the name
+        inpt_bools = [(i in expected_types)
+                      or (i in expected_names) or (i + 's' in expected_names)
                       for i in INPT_NAMES]
         pass_to_func = [i for b, inpt in zip(inpt_bools, [imgs, msks, trks, arrs])
                         for i in inpt if b]
@@ -110,11 +118,10 @@ class ImageHelper():
         return pass_to_func, args, kwargs
 
     def _pass_by_frames(self,
-                        func: Callable,
                         pass_to_func: List,
+                        nonarr_inputs: Tuple,
                         *args, **kwargs
                         ) -> List:
-
         # Use the output type to set the value of the array
         if self.dtype is not None:
             dtype = self.dtype
@@ -127,13 +134,17 @@ class ImageHelper():
         elif self.output_type.__name__ == 'track':
             dtype = np.uint16
         else:
-            raise TypeError(f'Was unable to determine type for output of {func}.')
+            raise TypeError('Was unable to determine type for '
+                            f'output of {self.func}.')
 
         # Initialize output array
+        # NOTE: This assumes that output is the same shape as input
+        #       and that all the inputs have the same shape
         out = np.empty(pass_to_func[0].shape).astype(dtype)
         for fr in range(out.shape[0]):
             # Pass each frame individually for all inputs
-            out[fr, ...] = func(*[p[fr, ...] for p in pass_to_func],
-                                *args, **kwargs)
+            out[fr, ...] = self.func(*nonarr_inputs,
+                                     *[p[fr, ...] for p in pass_to_func],
+                                     *args, **kwargs)
 
         return out
