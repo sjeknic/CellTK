cc321de67b7cc1e21c50b767d048f9306d1e21f7
sjeknic <sjeknic@stanford.edu>
2021-11-22 16:00:49 -0800

    added ability to surpress output

--- cellst/utils/utils.py
+++ cellst/utils/utils.py
@@ -1,6 +1,9 @@
+import sys
+import os
 import types
 import inspect
 import functools
+import contextlib
 from typing import List, Tuple
 
 import numpy as np
@@ -38,6 +41,39 @@ def custom_function(operation):
 
         return func
     return decorator
+
+
+def fileno(file_or_fd):
+    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
+    if not isinstance(fd, int):
+        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
+    return fd
+
+
+@contextlib.contextmanager
+def stdout_redirected(to=os.devnull, stdout=None):
+    """
+    https://stackoverflow.com/a/22434262/190597 (J.F. Sebastian)
+    Context manager to redirect outputs from non-CellST funcs to os.devnull
+    """
+    if stdout is None: stdout = sys.stdout
+
+    stdout_fd = fileno(stdout)
+    # copy stdout_fd before it is overwritten
+    with os.fdopen(os.dup(stdout_fd), 'wb') as copied:
+        stdout.flush()  # flush library buffers that dup2 knows nothing about
+        try:
+            os.dup2(fileno(to), stdout_fd)  # $ exec >&to
+        except ValueError:  # filename
+            with open(to, 'wb') as to_file:
+                os.dup2(to_file.fileno(), stdout_fd)  # $ exec > to
+        try:
+            yield stdout  # allow code to be run with the redirected stdout
+        finally:
+            # restore stdout to its previous value
+            # NOTE: dup2 makes stdout_fd inheritable unconditionally
+            stdout.flush()
+            os.dup2(copied.fileno(), stdout_fd)  # $ exec >&copied
 
 
 class ImageHelper():
