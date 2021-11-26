ddad3a69c7b172e4c60b56e606dfa78c7c760953
sjeknic <sjeknic@stanford.edu>
2021-11-26 12:58:23 -0800

    added more metrics

--- cellst/utils/metric_utils.py
+++ cellst/utils/metric_utils.py
@@ -1,8 +1,25 @@
 import numpy as np
 
+'''TODO: Add nan policy'''
+
 
 def median_intensity(mask, image) -> float:
-    """
-    Returns median intensity in region of interest
-    """
+    """Returns median intensity in region of interest."""
     return np.median(image[mask])
+
+
+def total_intensity(mask, image) -> float:
+    """Returns total intensity in region of interest."""
+    return np.sum(image[mask])
+
+
+def intensity_variance(mask, image) -> float:
+    """Returns variance of all intensity values in region of interest."""
+    return np.var(image[mask])
+
+
+def intensity_stdev(mask, image) -> float:
+    """
+    Returns standard deviation of all intensity values in region of interest.
+    """
+    return np.std(image[mask])
