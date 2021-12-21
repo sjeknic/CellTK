d8cbc1d64d06418af943ed747f5a94713353779d
sjeknic <sjeknic@stanford.edu>
2021-12-20 17:12:40 -0800

    updates to sherlock tests

--- cellst/utils/slurm_utils.py
+++ cellst/utils/slurm_utils.py
@@ -1,12 +1,22 @@
 import subprocess
+import logging
 
+class JobController():
+    __name__ = 'JobController'
 
-class SlurmController():
+    def set_logger(self, logger: logging.Logger) -> None:
+        """
+        """
+        # logger is either a Pipeline or Operation logger
+        log_name = logger.name
+
+        # This logs to same file, but records the Operation name
+        self.logger = logging.getLogger(f'{log_name}.{self.__name__}')
+
+class SlurmController(JobController):
+    __name__ = 'SlurmController'
     _line_offset = 1  # Number of lines squeue returns if n_jobs == 0
     _delay = 10  # Wait between commands in sec
-
-    def __init__(self) -> None:
-        pass
 
     def _check_user_jobs(self,
                          partition: str = '$GROUP',
@@ -23,24 +33,22 @@ class SlurmController():
 
     def _create_bash_script(self,
                             partition: str = '$GROUP',
-                            uid: str = '$USER',
                             job_name: str = 'cellst_pipe',
                             time: str = '24:00:00',
                             ntasks: str = '1',
                             cpus_per_task: str = '1',
-                            mem: str = '8GB'
+                            mem: str = '8GB',
+                            fname: str = '.temp.sh'
                             ) -> None:
         """
         Runs a bash script to submit a single job to the SLURM controller
         sbatch will return 0 on success or error code on failure.
         """
-        def _add_line(string: str,
-                      add: str,
+        def _add_line(add: str,
                       header: str = '#SBATCH',
                       div: str = ' '
                       ) -> str:
             string += f'\n{header}{div}{add}'
-            return string
 
         # Get the options for the script
         params = {l: v for l, v in locals().items()
@@ -51,15 +59,14 @@ class SlurmController():
         for p, val in params.items():
             pname = p.replace('_', '-')
             to_add = f'--{pname}={val}'
-            string = _add_line(string, to_add)
+            _add_line(to_add, div=' ')
 
-        string = _add_line(string, '', '', '')
+        # TODO: Is the blank line really needed?
+        _add_line('', '', '')
 
         # TODO: Add the real python command here
         mods = 'module load cellst376'
-        string = _add_line(string, mods, '', '')
+        _add_line(mods, '', '')
 
-        command = 'python -m cellst.pipeline -y /home/users/sjeknic/CellST/test_out_orch/pipeline_yamls/F3-Site_3.yaml'
-        string = _add_line(string, command, '', '')
-
-        subprocess.run([string], shell=True)
+        command = 'python -m cellst.pipeline -y home/groups/sjeknic/CellST/pipeline_yamls/pipeline_yamls/F5-Site_1.yaml'
+        _add_line(command, '', '')
