260b685902160e34cef9e14ef9dd920462911991
sjeknic <sjeknic@stanford.edu>
2022-01-23 14:00:20 -0800

    added more slurm monitoring

--- cellst/utils/slurm_utils.py
+++ cellst/utils/slurm_utils.py
@@ -107,8 +107,28 @@ class SlurmController(JobController):
             self.logger.info(f'Submitted {self.pipes_run + 1} out '
                              f'of {self.total_pipes} pipelines.')
             sleep(10 * self._submit_delay)
+            self._record_job_history(update=True)
+            self.logger.info(f'Current state: \n{self.status}')
 
         self.logger.info('Finished running jobs.')
+
+    @property
+    def status(self) -> str:
+        """"""
+        states = dict(S='Submitted', R='Running', C='Complete',
+                      F='Failed', K='Killed')
+
+        display_string = f'Total: {self.total_pipes} \n'
+        total = 0
+        for key, state in states.items():
+            num = len([v for v in self.job_history.values()
+                       if v['state'] == key])
+            display_string += f'{state}: {num} \n'
+            total += num
+
+        display_string += f'Accounted for: {total} / {self.total_pipes}'
+
+        return display_string
 
     def _make_slurm_kwargs(self,
                            partition: List,
@@ -169,7 +189,7 @@ class SlurmController(JobController):
 
         return True
 
-    def _yield_working_sbatch(self, pipelines: dict):
+    def _yield_working_sbatch(self, pipelines: dict) -> str:
         self.logger.info(f'Building {len(pipelines)} pipelines: {list(pipelines.keys())}')
 
         for self.pipes_run, (fol, kwargs) in enumerate(pipelines.items()):
@@ -211,11 +231,14 @@ class SlurmController(JobController):
                                             name=name, output=output_dir)
 
         # Updates are slow, so don't do them often
-        update = True
         if update:
             if time_module.time() - self.last_update_time > self._update_delay:
+                # Parse status from current jobs - only R should update
                 current_jobs = self._get_slurm_info(keys)
+                current_jobs = self._use_only_valid_states(current_jobs)
+                self.job_history.update(current_jobs)
                 self.last_update_time = time_module.time()
+
                 ended_jobs = [k for k in self.job_history
                               if k not in current_jobs]
                 self._check_ended_job_status(ended_jobs)
@@ -225,15 +248,19 @@ class SlurmController(JobController):
         Check the logs of the given jobs to see if the Pipeline is complete.
         """
         for j in jobs:
-            logfile = os.path.join(self.job_history[j]['output'], 'log.txt')
-            last_line = get_file_line(logfile, -1)
+            # Check if already recorded
+            if self.job_history[j]['state'] not in ('C', 'F', 'K'):
+                # Check log file for state of the Pipeline
+                logfile = os.path.join(self.job_history[j]['output'],
+                                       'log.txt')
+                last_line = get_file_line(logfile, -1)
 
-            if 'Pipeline completed.' in last_line:
-                self.job_history[j]['state'] = 'C'  # Complete
-            elif 'completed by Pipeline.' in last_line:
-                self.job_history[j]['state'] = 'P'  # Partial
-            else:
-                self.job_history[j]['state'] = 'K'  # Killed
+                if 'Pipeline completed.' in last_line:
+                    self.job_history[j]['state'] = 'C'  # Complete
+                elif 'completed by Pipeline.' in last_line:
+                    self.job_history[j]['state'] = 'F'  # Failed
+                else:
+                    self.job_history[j]['state'] = 'K'  # Killed
 
     def _check_user_jobs(self,
                          partition: str = '$GROUP',
@@ -275,13 +302,28 @@ class SlurmController(JobController):
         format_string = ':>,'.join([key_to_format[k] for k in keys])
         command = f'squeue -p {partition} -u {user} -h --Format="{format_string}"'
         result = subprocess.run([command], shell=True, capture_output=True, text=True)
-
         # Parse the output string
         result = [r.rstrip().split('>') for r in result.stdout.split('\n')
                   if r != '']
         current_jobs = {r[0]: {k: v for k, v in zip(keys, r)}
                                for r in result}
+
         return current_jobs
+
+    def _use_only_valid_states(self, jobs: dict) -> dict:
+        """Remove states (e.g.CF) that aren't in self.status"""
+        for j, val in jobs.items():
+            if val['state'] in ('PD', 'CF'):
+                jobs[j]['state'] = 'S'
+            elif val['state'] in ('CG'):
+                # will get updated by _check_ended
+                jobs[j]['state'] = 'R'
+            elif val['state'] in ('R'):
+                pass
+            else:
+                print('STATE NOT UNDERSTOOD')
+
+        return jobs
 
     def _create_bash_script(self,
                             partition: str = '$GROUP',
