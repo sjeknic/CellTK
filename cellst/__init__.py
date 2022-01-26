322c66855f598924b90ce9b638b474873f2645a7
sjeknic <sjeknic@stanford.edu>
2022-01-25 16:52:15 -0800

    quick fix for bug preventing use of Track

--- cellst/__init__.py
+++ cellst/__init__.py
@@ -11,15 +11,6 @@ from .extract import Extract
 from .evaluate import Evaluate
 
 # Import important types
-'''
-TODO: One of two things has to be True. Either _types has to
-not contain the _types that users will actually use (think Condition, Experiment).
-OR the non_user facing items in _types must be changed to private.
-
-I don't want to go too crazy, but I do think some restructuring and consolidating
-of utils is warranted. Hopefully I can get around to that tonight.
-'''
 from .utils import _types as cst_types
-from .utils._types import Image, Mask, Track, Arr, Same
 from .utils._types import Condition, Experiment
 from .utils.slurm_utils import SlurmController
