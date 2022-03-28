9a8838b1cfc7f6f7175309cb3ab5b34eb67e1167
sjeknic <sjeknic@stanford.edu>
2022-03-28 08:28:19 -0700

    Added stand-alone predict_peaks function. Should work for any 2D array

--- celltk/utils/metric_utils.py
+++ celltk/utils/metric_utils.py
@@ -1,6 +1,10 @@
-import warnings
+from typing import Union, Tuple
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+from celltk.utils.unet_model import UPeakModel
+from celltk.utils.plot_utils import plot_trace_predictions
 
 
 def median_intensity(mask: np.ndarray, image: np.ndarray) -> float:
@@ -39,8 +43,45 @@ def cumulative_active(array: np.ndarray) -> np.ndarray:
 
 
 def predict_peaks(array: np.ndarray,
-                  weight_path: str = 'celltk/config/example_peak_weights.tf',
+                  weight_path: str = 'celltk/config/upeak_example_weights.tf',
                   segment: bool = True,
+                  roi: Union[int, Tuple[int]] = (1, 2),
+                  save_path: str = None,
+                  save_plot: str = None,
+                  show_plot: bool = False
                   ) -> None:
-    """Placeholder function for now, but can change to include peaak prediction"""
-    pass
+    """Predicts peaks in arbitrary data using UPeak.
+
+    :param array: Data of shape n_samples x n_timepoints. Each sample will
+        have peaks predicted.
+    :param weight_path: Weights to use for predicting peaks. If not provided,
+        uses default weights.
+    :param segment: If True, segments peaks using a watershed based algorithm
+        and returns segmennted peaks instead of peak probabilities.
+    :param roi: Regions of interest to return. 0 is background, 1 is slope, and
+        2 is plateau.
+    :param save_path: If provided, saves the output array at the supplied path.
+    :param save_plot: If a string is provided, saves output figures using the given
+        figure name. Do not provided extension in file name, figures can currently
+        only be saved as png.
+    :param show_plot:
+    """
+    # Get model
+    model = UPeakModel(weight_path)
+    predictions = model.predict(array, roi=roi)
+
+    if segment:
+        # This will overwrite predictions, the segmentation is returned/plotted
+        pass
+
+    # Save the outputs
+    if save_path:
+        np.save(save_path, predictions)
+
+    if save_plot or show_plot:
+        for fidx, fig in enumerate(plot_trace_predictions(array, predictions)):
+            if save_plot: plt.savefig(fig, f'{save_plot}_{fidx}.png')
+            if show_plot: plt.show()
+            plt.close(fig)
+
+    return predictions
