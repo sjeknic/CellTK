2a0388a52d4eede1fd4cc9a5f8b6ab5927fcc328
sjeknic <sjeknic@stanford.edu>
2022-03-31 17:47:24 -0700

    cleanup

--- kit_sch_ge/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-"""TODO: Would prefer to do this without having to change sys.path
-couldn't quite figure it out. Maybe relative imports"""
-
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
