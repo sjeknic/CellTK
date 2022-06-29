f11e3f1ec41595b133b73a6106057e7d486cd429
sjeknic <sjeknic@stanford.edu>
2022-06-29 13:33:10 -0700

    Version bump

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Stevan Jeknic'
 author = 'Stevan Jeknic'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.0'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="celltk",
-    version="0.4.0",
+    version="0.4.1",
     author="Stevan Jeknic",
     author_email="sjeknic@stanford.edu",
     description="A tool kit for working with large amounts of live-cell microscopy data.",
@@ -28,7 +28,7 @@ setuptools.setup(
     install_requires=[
         'setuptools>=41.2.0',
         'tensorflow>=2.7.0,<2.9.0',
-        'numpy>=1.20',
+        'numpy>=1.20,<1.23',  # Upper bound on version is from umap-learn?
         'scipy>=1.6.3',
         'scikit_learn>=1.0.1',
         'scikit_image>=0.19.1',
@@ -39,16 +39,13 @@ setuptools.setup(
         'gurobipy>=9.5.0',
         'h5py>=3.6.0',
         'imageio>=2.13.0',
-        'mahotas>=1.4.5',
+        'mahotas>=1.4.5,<1.4.13',  # Upperbound: fails to import mahotas.segmentation
         'napari>=0.4.12',
         'PyYAML>=6.0',
         'SimpleITK>=2.1.1',
         'tifffile>=2021.11.2',
-        'Bottleneck>=1.3.2',
         'btrack>=0.4.2',
         'colorcet~=3.0.0',
-        'umap-learn>=0.5.3',
-        'hdbscan~=0.8.28',
         'seaborn~=0.11.2'
     ],
     python_requires=">=3.8",
