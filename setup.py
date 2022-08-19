47d31dff72ba49c5b03aecba7e0a6781d59948f0
sjeknic <sjeknic@stanford.edu>
2022-08-19 15:09:56 -0700

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="celltk",
-    version="0.4.1",
+    version="0.4.2",
     author="Stevan Jeknic",
     author_email="sjeknic@stanford.edu",
     description="A tool kit for working with large amounts of live-cell microscopy data.",
